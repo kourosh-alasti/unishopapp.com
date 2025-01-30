67b20dae754adc0b3311802204887a8a133e1696
Kourosh Alasti <coding@kouroshalasti.com>
2025-01-30 02:33:26 -0800

    fix: light/dark/system themes

--- src/components/provider.tsx
+++
@@ -1,7 +0,0 @@
-import { ThemeProvider } from "next-themes";
-
-export default function Provider({ children, ...props} : React.ComponentProps<typeof ThemeProvider>) {
-    return (<ThemeProvider enableSystem={false} attribute={"class"} disableTransitionOnChange>
-        {children}
-    </ThemeProvider>)
-}
