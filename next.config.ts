bdd8c37f01cf672c8f92e1e3c68466a94d48a5a0
Kourosh Alasti <coding@kouroshalasti.com>
2025-01-29 23:59:13 -0800

    fix: incorrect imports

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-import './src/env/server.js';
-import './src/env/client.js';
+import './src/env/server.ts';
+import './src/env/client.ts';
 
 const nextConfig: NextConfig = {
   /* config options here */
