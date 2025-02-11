14fe8a2b377ab8270dc3d599c0297763c17e30d4
Kourosh Alasti <coding@kouroshalasti.com>
2025-02-10 22:52:31 -0800

    fix: remove url validation

--- src/env/client.ts
+++ src/env/client.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 
 export const env = createEnv({
     client: {
-        NEXT_PUBLIC_APP_URL: z.string().url(),
+        NEXT_PUBLIC_APP_URL: z.string(),
     },
     runtimeEnv: {
         NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_URL,
